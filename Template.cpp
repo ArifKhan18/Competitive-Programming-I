aaf64fa842efac8b89e2c067f7781e8c2df5dc22
MD. ARIF KHAN <130598988+ArifKhan18@users.noreply.github.com>
2025-01-03 19:31:04 +0600

    Add files via upload

---
+++ Template.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll   =  long long;
+using ull  =  unsigned long long;
+using ld   =  long double;
+
+#define endl            '\n'
+#define all(x)          (x).begin(), (x).end()
+#define rall(x)         (x).rbegin(), (x).rend()
+#define point(x)        fixed << setprecision(x)
+#define FastIo          ios_base::sync_with_stdio(false); cin.tie(nullptr)
+#define sz(x)           (int)x.size()
+
+const ld pi = acos(-1);
+const ll mod = 1e9 + 7;
+const ll mxn = 1e5 + 5;
+
+void Plz_Ac() {
+
+}
+
+int main() {
+    FastIo;
+    int test = 1;
+    //cin >> test;
+    while (test--) {
+        Plz_Ac();
+    }
+    return 0;
+}
